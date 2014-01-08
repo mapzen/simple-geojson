d70ac6c405c378aee92c5b3300431b5448b3fad5
mjcunningham <michaeljcunningham@gmail.com>
2014-01-08 16:18:51 -0500

    Updating strings for navigation instructions
    Trying to make it read like a sentence.

--- src/main/java/com/mapzen/osrm/Instruction.java
+++ src/main/java/com/mapzen/osrm/Instruction.java
@@ -4,24 +4,24 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 public class Instruction {
-    public static String NoTurn = "No Turn"; // 0;          //Give no instruction at all
-    public static String GoStraight = "Go Straight"; //1;      //Tell user to go straight!
-    public static String TurnSlightRight = "Turn Slight Right"; //2;
-    public static String TurnRight = "Turn Right"; // 3;
-    public static String TurnSharpRight = "Turn Sharp Right"; // 4;
+    public static String NoTurn = "No turn"; // 0;          //Give no instruction at all
+    public static String GoStraight = "Continue on"; //1;      //Tell user to go straight!
+    public static String TurnSlightRight = "Make a slight right on to"; //2;
+    public static String TurnRight = "Make a right on to"; // 3;
+    public static String TurnSharpRight = "Make a sharp right on to"; // 4;
     public static String UTurn = "U Turn"; // 5;
-    public static String TurnSharpLeft = "Turn Sharp Left"; // 6;
-    public static String TurnLeft = "Turn Left"; // 7;
-    public static String TurnSlightLeft = "Turn Slight Left"; // 8;
-    public static String ReachViaPoint = "Reach Via Point"; // 9;
-    public static String HeadOn = "Head On"; // 10;
-    public static String EnterRoundAbout = "Enter Round About"; // 11;
-    public static String LeaveRoundAbout = "LEave Round About"; // 12;
-    public static String StayOnRoundAbout = "Stay On Round About"; // 13;
-    public static String StartAtEndOfStreet = "Start At End Of Street"; // 14;
-    public static String ReachedYourDestination = "Reached Your Destination"; // 15;
-    public static String EnterAgainstAllowedDirection = "Enter Against Allowed Direction"; // 16;
-    public static String LeaveAgainstAllowedDirection = "Leave Against Allowed Direction"; // 17;
+    public static String TurnSharpLeft = "Make a sharp left on to"; // 6;
+    public static String TurnLeft = "Make a left on to"; // 7;
+    public static String TurnSlightLeft = "Make a slight left on to"; // 8;
+    public static String ReachViaPoint = "Reach via point"; // 9;
+    public static String HeadOn = "Head on"; // 10;
+    public static String EnterRoundAbout = "Enter round about"; // 11;
+    public static String LeaveRoundAbout = "Leave round about"; // 12;
+    public static String StayOnRoundAbout = "Stay on round about"; // 13;
+    public static String StartAtEndOfStreet = "Start at end of street"; // 14;
+    public static String ReachedYourDestination = "You have reached your destination"; // 15;
+    public static String EnterAgainstAllowedDirection = "Enter against allowed direction"; // 16;
+    public static String LeaveAgainstAllowedDirection = "Leave against allowed direction"; // 17;
 
     public static int METERS_IN_MILE = 1609;
 
